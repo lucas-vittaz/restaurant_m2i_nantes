87dfcc14bdf6923f68010a3450fcc98fcd1b7986
lucas.vittaz <lucasvittaz.pro@gmail.com>
2023-04-26 13:38:56 +0200

    import card component in restaurant

--- src/app/restaurants/restaurants.module.ts
+++ src/app/restaurants/restaurants.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RestaurantsRoutingModule } from './restaurants-routing.module';
+import { PageListRestaurantsComponent } from './pages/page-list-restaurants/page-list-restaurants.component';
+import { PageEditRestaurantComponent } from './pages/page-edit-restaurant/page-edit-restaurant.component';
+import { PageAddRestaurantComponent } from './pages/page-add-restaurant/page-add-restaurant.component';
+import { SharedModule } from '../shared/shared.module';
+import { PageShowRestaurantComponent } from './pages/page-show-restaurant/page-show-restaurant.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PageListRestaurantsComponent,
+    PageEditRestaurantComponent,
+    PageAddRestaurantComponent,
+    PageShowRestaurantComponent
+  ],
   imports: [
     CommonModule,
-    RestaurantsRoutingModule
+    RestaurantsRoutingModule, SharedModule
   ]
 })
 export class RestaurantsModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TemplatesModule } from '../templates/templates.module';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CardComponent
+  ],
   imports: [CommonModule, RouterModule],
-  exports: [TemplatesModule],
+  exports: [TemplatesModule, CardComponent],
 })
 export class SharedModule {}
