3692d33abbf68238900c583e7b7c2d50bb25095f
lucas.vittaz <lucasvittaz.pro@gmail.com>
2023-04-26 13:38:38 +0200

    add httpclientmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,7 +9,13 @@ import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CoreModule, AppRoutingModule, FontAwesomeModule],
+  imports: [
+    BrowserModule,
+    CoreModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    HttpClientModule,
+    ],
   exports: [],
   providers: [],
   bootstrap: [AppComponent],
